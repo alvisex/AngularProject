f60287604a82d240a7c0042670cd952a8d75fc4d
Alvisex <alvise.ld@gmail.com>
2018-05-21 19:34:19 -0500

    Agregue el método Angular (componente) para obtener uno solo en base a _id

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute} from '@angular/router';
 import {Product} from '../product';
 import {ProductService} from '../product.service';
 
+import {ServiciodbbService} from '../serviciodbb.service';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -11,25 +13,39 @@ import {ProductService} from '../product.service';
 })
 export class DetailsComponent implements OnInit {
 @Input() product: Product;
-
-  constructor(private location: Location,
+serverRes: any;
+  constructor(
+  private location: Location,
   private route: ActivatedRoute,
+  private productService: ProductService,
 
-  private productService: ProductService
-  
+  private servicio: ServiciodbbService
   ) { }
 
   ngOnInit() {
   this.getProduct();
-  
+
   }
-  
+
   getProduct():void{
   const id=+ this.route.snapshot.paramMap.get('id');
   this.productService.getProduct(id)
   .subscribe(product => this.product = product);
   }
-  
+
+  getProductBD() {
+    // const id = +this.route.snapshot.paramMap.get('id');
+    const id = this.route.snapshot.paramMap.get('id');
+    const cn = 'products';
+
+    this.servicio.getOneP(id, cn).subscribe( res => {
+      this.serverRes = res ;
+      // console.log(this.serverRes);
+      this.product = this.serverRes.response.data[0];
+      console.log(this.product);
+    });
+  }
+
     goBack(): void{
   this.location.back();
   }
